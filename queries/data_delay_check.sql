f53ed8344b69825b01d39e2bf929168a6fa4a158
Kia Somabes <kia.somabes@amaysim.com.au>
2023-06-01 16:30:10 +0800

    add custom query

---
+++ queries/data_delay_check.sql
@@ -0,0 +1,8 @@
+select * from amaysimdw.monitoring.compass_rs_sf_count_check 
+where 
+-- temporarily trying with previous 100 days since there's no data for a while
+jobruntime >= date_trunc(day, current_date-100) 
+AND (DR_COUNT <> SF_COUNT
+OR  SF_COUNT <> COMPASS_COUNT
+OR DR_COUNT <> COMPASS_COUNT)
+order by JOBRUNTIME desc; 
