75aa582d5ee8e1e110efd2fc5c00089539a710bd
Kia Somabes <kia.somabes@amaysim.com.au>
2023-06-06 14:16:19 +0800

    Update queries and add metadata change check

--- queries/longest_queries.sql
+++ queries/longest_queries.sql
@@ -4,11 +4,14 @@ CASE
  WHEN QUERY_TYPE = 'INSERT' THEN '<redacted>'
  ELSE QUERY_TEXT
 END AS QUERY_TEXT,
+
+-- EXECUTION_TIME -> in milliseconds
+-- EXEC_TIME -> in minutes
 (EXECUTION_TIME / 60000) as EXEC_TIME,
+
 WAREHOUSE_NAME,
 USER_NAME,
 EXECUTION_STATUS,
 START_TIME
 from "SNOWFLAKE"."ACCOUNT_USAGE"."QUERY_HISTORY" 
-where EXECUTION_STATUS = 'SUCCESS' 
-and start_time >= dateadd('hour',-1,current_timestamp());
+where start_time >= DATE('2023-06-01');
