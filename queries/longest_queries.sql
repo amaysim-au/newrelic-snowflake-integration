1bff61d5e22027b0ff66485fbac33cf48cc0d8d1
Kia Somabes <kia.somabes@amaysim.com.au>
2023-06-08 08:51:58 +0800

    modify longest_queries filter

--- queries/longest_queries.sql
+++ queries/longest_queries.sql
@@ -14,4 +14,5 @@ USER_NAME,
 EXECUTION_STATUS,
 START_TIME
 from "SNOWFLAKE"."ACCOUNT_USAGE"."QUERY_HISTORY" 
-where start_time >= date_trunc(day, current_date);
+where start_time >= date_trunc(day, current_date) 
+AND EXEC_TIME >= 30;
