db422b49847b72848813e229c28429838c46a371
Kia Somabes <kia.somabes@amaysim.com.au>
2023-06-14 11:58:34 +0800

    Update filter for data_delay_check to current + yesteday date and modify flex

--- queries/data_delay_check.sql
+++ queries/data_delay_check.sql
@@ -1,7 +1,7 @@
 select * from amaysimdw.monitoring.compass_rs_sf_count_check 
 where 
 -- temporarily trying with previous 100 days since there's no data for a while
-jobruntime >= date_trunc(day, current_date-100) 
+jobruntime >= date_trunc(day, current_date-1) 
 AND (DR_COUNT <> SF_COUNT
 OR  SF_COUNT <> COMPASS_COUNT
 OR DR_COUNT <> COMPASS_COUNT)
